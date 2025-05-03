cb8d2c631200a80c4d262d3133eaa9bc8503a752
Pranav Bhatkar <pranavbhatkar789@gmail.com>
2025-05-03 12:57:08 +0530

    refactor(database): remove @neondatabase/serverless integration and simplify drizzle initialization

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,8 @@
 import { env } from "@/env";
-import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { waitlist, foundingMember } from "@/db/schema";
-const sql = neon(env.DATABASE_URL);
-export const db = drizzle({
-  client: sql,
+
+export const db = drizzle(env.DATABASE_URL, {
   schema: {
     waitlist,
     foundingMember,
